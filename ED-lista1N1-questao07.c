70b1ec8004333dc2e05a759699ec29bf2146bbb3
davidrtc14 <davidrtc14@gmail.com>
2024-04-01 16:48:15 -0300

    Resolução Questão 07

--- ED-lista1N1-questao07.c
+++ ED-lista1N1-questao07.c
@@ -1,6 +1,48 @@
 /*
-**    Função :
+**    Função : Verificar e exibir números felizes e não felizes
 **    Autor : David Ramalho Teixeira de Carvalho
-**    Data  : 30/03/2024
+**    Data  : 01/04/2024
 **    Observações:
 */
+#include <stdio.h>
+#include <locale.h>
+
+
+int soma_quadrados_digitos(int n) {
+    int soma = 0;
+    while (n != 0) {
+        int digito = n % 10;
+        soma += digito * digito;
+        n /= 10;
+    }
+    return soma;
+}
+
+int eh_numero_feliz(int n) {
+    int visitados[1000] = {0}; 
+
+    while (1) {
+        if (n == 1)
+            return 1; 
+        if (visitados[n])
+            return 0; 
+        visitados[n] = 1; 
+        n = soma_quadrados_digitos(n); 
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese"); 
+
+    int num;
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &num);
+
+    if (eh_numero_feliz(num)) {
+        printf("%d é um número feliz.\n", num);
+    } else {
+        printf("%d não é um número feliz.\n", num);
+    }
+
+    return 0;
+}
